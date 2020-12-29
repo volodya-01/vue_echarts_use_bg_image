2853e9b927e3b96421670ccdecc841eecad4fee9
Ztdata--niubao <niubao@chitechdata.cn>
2020-12-29 16:41:46 +0800

    添加标题

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,55 +1,55 @@
 <template>
   <div class="hello">
     <div class="echartsbox">
-      <div>1</div>
+      <div class="echartsbox_Title">1.填充图片背景1</div>
       <echarts1 />
     </div>
     <div class="echartsbox">
-      <div>2</div>
+     <div class="echartsbox_Title">2.填充图片背景2</div>
       <echarts2 />
     </div>
     <div class="echartsbox">
-      <div>3</div>
+  <div class="echartsbox_Title">3.填充图片背景3</div>
       <echarts3 />
     </div>
-     <div class="echartsbox">
-       <div>4</div>
+    <div class="echartsbox">
+     <div class="echartsbox_Title">4.柱状图每个柱子单独设置颜色</div>
       <echarts4 />
     </div>
-     <div class="echartsbox">
-       <div>5</div>
+    <div class="echartsbox">
+       <div class="echartsbox_Title">5.多坐标系联动1</div>
       <echarts5 />
     </div>
-     <div class="echartsbox">
-       <div>6</div>
+    <div class="echartsbox">
+     <div class="echartsbox_Title">6.多坐标系联动2</div>
       <echarts6 />
     </div>
-      <div class="echartsbox">
-        <div>7</div>
+    <div class="echartsbox">
+       <div class="echartsbox_Title">7.水泵流量压力曲线</div>
       <echarts7 />
     </div>
-     <div class="echartsbox">
-       <div>72</div>
+    <div class="echartsbox">
+      <div class="echartsbox_Title">72.水泵曲线</div>
       <echarts72 />
     </div>
-     <div class="echartsbox">
-       <div>8</div>
+    <div class="echartsbox">
+       <div class="echartsbox_Title">8.柱状图左右都有标签</div>
       <echarts8 />
     </div>
     <div class="echartsbox">
-      <div>9</div>
+         <div class="echartsbox_Title">9.象形图</div>
       <echarts9 />
     </div>
-     <div class="echartsbox">
-       <div>10</div>
+    <div class="echartsbox">
+          <div class="echartsbox_Title">10.网格线</div>
       <echarts10 />
     </div>
-     <div class="echartsbox">
-       <div>11</div>
+    <div class="echartsbox">
+       <div class="echartsbox_Title">11.组合立体柱状图</div>
       <echarts11 />
     </div>
-     <div class="echartsbox">
-       <div>12</div>
+    <div class="echartsbox">
+       <div class="echartsbox_Title">12.组合立体柱状图</div>
       <echarts12 />
     </div>
   </div>
@@ -86,79 +86,79 @@ export default {
     echarts9,
     echarts10,
     echarts11,
-    echarts12
+    echarts12,
   },
-  mounted(){
-    this.WaterworksDispatchCurveData()
+  mounted() {
+    this.WaterworksDispatchCurveData();
   },
-  methods:{
-   WaterworksDispatchCurveData(){
-          this.$axios.post("/api/WaterworksDispatchCurveData",
-          {
-            headers: { "Content-Type": "application/json;" },
-          }
-        )
+  methods: {
+    WaterworksDispatchCurveData() {
+      this.$axios
+        .post("/api/WaterworksDispatchCurveData", {
+          headers: { "Content-Type": "application/json;" },
+        })
         .then((res) => {
           console.log("3.	获取调度对比曲线 WaterworksDispatchCurveData ", res);
-           let ActualMaxData= res.data.ActualMaxData
-            let OptimalMaxData= res.data.OptimalMaxData
-           let ActualDispatchData =[]//当前
-             let OptimalDispatchData=[]//调度
-            //FlowData PressureData PumpCurveData
-            //当前
-            let Actual_Time=[]
-            let Actual_FlowData=[]
-            let Actual_PressureData=[]
-            let Actual_PumpCurveData=[]
-            let PumpNoArray=[]
-            res.data.ActualDispatchData.FlowData.map(ele=>{
-              Actual_Time.push(ele.Time)
-              Actual_FlowData.push(ele.Data)
-              }
-            )
-            //水泵
-            res.data.ActualDispatchData.PressureData.map(ele=>{
-              Actual_PressureData.push(ele.Data)
-              }
-            )
-            /*  */
-            //  res.data.ActualDispatchData.PumpCurveData.forEach(ele=>{
-             
-            //   ele.PumpData.map((el) => {
-            //    el.Data=(el.Data+6)*60
-            //   })
-              
-            //   }
-            // )
-            /*  */
-             res.data.ActualDispatchData.PumpCurveData.map(ele=>{
-               PumpNoArray.push(ele.PumpNo)
-              let PumpData=[]
-              let PumpDataNull=[]
-              let Time=[]
-              ele.PumpData.map((el) => {
-                PumpData.push(Number(el.Data)<0?"":el.Data)
-                PumpDataNull.push(Number(el.Data)<0?el.Data:"")
-                Time.push(el.Time)
-              })
-                Actual_PumpCurveData.push({
-                  PumpData,
-                  Time,
-                  PumpNo:ele.PumpNo,
-                  PumpDataNull
-                  
-                })
-              }
-            )
-            console.log("Actual_PumpCurveData",Actual_PumpCurveData)
-         
-                  let ActualData={Actual_FlowData,Actual_PressureData,Actual_PumpCurveData,Actual_Time}
-/*  */
-         Bus.$emit("WaterworksDispatchCurveDataTo", ActualData);
+          let ActualMaxData = res.data.ActualMaxData;
+          let OptimalMaxData = res.data.OptimalMaxData;
+          let ActualDispatchData = []; //当前
+          let OptimalDispatchData = []; //调度
+          //FlowData PressureData PumpCurveData
+          //当前
+          let Actual_Time = [];
+          let Actual_FlowData = [];
+          let Actual_PressureData = [];
+          let Actual_PumpCurveData = [];
+          let PumpNoArray = [];
+          res.data.ActualDispatchData.FlowData.map((ele) => {
+            Actual_Time.push(ele.Time);
+            Actual_FlowData.push(ele.Data);
+          });
+          //水泵
+          res.data.ActualDispatchData.PressureData.map((ele) => {
+            Actual_PressureData.push(ele.Data);
+          });
+          /*  */
+          //  res.data.ActualDispatchData.PumpCurveData.forEach(ele=>{
+
+          //   ele.PumpData.map((el) => {
+          //    el.Data=(el.Data+6)*60
+          //   })
+
+          //   }
+          // )
+          /*  */
+          res.data.ActualDispatchData.PumpCurveData.map((ele) => {
+            PumpNoArray.push(ele.PumpNo);
+            let PumpData = [];
+            let PumpDataNull = [];
+            let Time = [];
+            ele.PumpData.map((el) => {
+              PumpData.push(Number(el.Data) < 0 ? "" : el.Data);
+              PumpDataNull.push(Number(el.Data) < 0 ? el.Data : "");
+              Time.push(el.Time);
+            });
+            Actual_PumpCurveData.push({
+              PumpData,
+              Time,
+              PumpNo: ele.PumpNo,
+              PumpDataNull,
+            });
+          });
+          console.log("Actual_PumpCurveData", Actual_PumpCurveData);
+
+          let ActualData = {
+            Actual_FlowData,
+            Actual_PressureData,
+            Actual_PumpCurveData,
+            Actual_Time,
+          };
+          /*  */
+          Bus.$emit("WaterworksDispatchCurveDataTo", ActualData);
         })
         .catch((error) => {});
-   }
-  }
+    },
+  },
 };
 </script>
 
@@ -168,5 +168,11 @@ export default {
   width: 60%;
   height: 760px;
   margin: 60px 220px;
+  .echartsbox_Title {
+    font: bold 24px "微软雅黑";
+    padding: 20px 0;
+    width: 100%;
+    text-align: start;
+  }
 }
 </style>
